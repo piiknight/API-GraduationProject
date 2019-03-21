a2315fa87803194edac12c8af1b4d683397744a5
Dien <tmtdien266@gmail.com>
2019-03-21 20:42:03 +0700

    First commit

---
+++ app/models/user-model.js
@@ -0,0 +1,85 @@
+var db = require('../../config/database');
+var dbFunc = require('../../config/db-function');
+
+var userModel = {
+   getAllUser:getAllUser,
+   addUser:addUser,
+   updateUser:updateUser,
+   deleteUser:deleteUser,
+   getUserById:getUserById
+}
+
+function getAllUser() {
+    return new Promise((resolve,reject) => {
+        db.query("SELECT * FROM user",(error,rows,fields)=>{
+            if(!!error) {
+                dbFunc.connectionRelease;
+                reject(error);
+            } else {
+                dbFunc.connectionRelease;
+                resolve(rows);
+            }
+       });
+    });
+}
+
+function getUserById(id) {
+    return new Promise((resolve,reject) => {
+        db.query("SELECT * FROM user WHERE idU ="+id.id,(error,rows,fields)=>{
+            if(!!error) {
+                dbFunc.connectionRelease;
+                reject(error);
+            } else {
+                dbFunc.connectionRelease;
+                resolve(rows);
+            }
+       });
+    });  
+}
+
+function addUser(user) {
+     return new Promise((resolve,reject) => {
+         db.query("INSERT INTO user(name,point,phone,email,address)VALUES('"+user.name+"','"+user.point+"','"+user.phone+"','"+user.email+"','" + user.address + "')",(error,rows,fields)=>{
+            if(error) {
+                dbFunc.connectionRelease;
+                reject(error);
+            } else {
+                dbFunc.connectionRelease;
+                resolve(rows);
+            }
+          });
+        });
+}
+
+
+function updateUser(id,user) {
+    return new Promise((resolve,reject) => {
+        db.query("UPDATE user set name='"+user.name+"',age='"+user.age+"',state='"+user.state+"',country='"+user.country+"' WHERE idU='"+id+"'",(error,rows,fields)=>{
+            if(!!error) {
+                dbFunc.connectionRelease;
+                reject(error);
+            } else {
+                dbFunc.connectionRelease;
+                resolve(rows);
+            }
+       });    
+    })
+}
+
+function deleteUser(id) {
+   return new Promise((resolve,reject) => {
+        db.query("DELETE FROM user WHERE idU='"+id+"'",(error,rows,fields)=>{
+            if(!!error) {
+                dbFunc.connectionRelease;
+                reject(error);
+            } else {
+                dbFunc.connectionRelease;
+                resolve(rows);
+            }
+       });    
+    });
+}
+
+
+module.exports = userModel;
+
